661abc9aa3b54a5142c631757a2573984e18dd6d
Leonardo <leonardo.smendescastilho@gmail.com>
2025-09-07 12:09:05 -0300

    feat: add the stardard function

--- routes/auth.py
+++ routes/auth.py
@@ -2,3 +2,11 @@ from fastapi import APIRouter
 
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
+
+
+@auth_router.get('/')
+async def authentication():
+    '''
+    Essa é a rota padrão de autentificação.
+    '''
+    return {"message": "Você acessou a rota de autentificação", "auth": False}
--- routes/order.py
+++ routes/order.py
@@ -2,3 +2,11 @@ from fastapi import APIRouter
 
 
 order_router = APIRouter(prefix="/order", tags=["order"])
+
+
+@order_router.get('/')
+async def orders():
+    '''
+    Essa é a rota padrão de pedidos do nosso sistema. Todas as rotas precisam de autentificação para acessar nosso sistema.
+    '''
+    return {"message": "você acessou a rota de pedidos"}
